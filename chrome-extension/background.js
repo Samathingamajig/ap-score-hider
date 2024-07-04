8e70612d03455c098a4a3531c6ff19c2a0e9cbd3
thepwrtank18 <pt18.relay@gmail.com>
2024-07-04 00:21:16 -0500

    firefox support

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,13 +1,15 @@
+var browser = browser || chrome;
+
 // Event listener for extension installation
-chrome.runtime.onInstalled.addListener(function (details) {
+browser.runtime.onInstalled.addListener(function (details) {
   if (details.reason === "install") {
     // Open options/index.html file in a new tab
-    chrome.tabs.create({ url: "options/index.html?justInstalled=true" });
+    browser.tabs.create({ url: "options/index.html?justInstalled=true" });
   }
 });
 
 // Event listener for extension icon click
-chrome.action.onClicked.addListener(function () {
+browser.action.onClicked.addListener(function () {
   // Open options/index.html file in a new tab
-  chrome.tabs.create({ url: "options/index.html" });
+  browser.tabs.create({ url: "options/index.html" });
 });
